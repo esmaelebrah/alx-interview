ada78341319149c081ae4b391dd2c78d29e63c89
esmaelebrah <esmaelebrahimhasengmail.com>
2024-04-25 01:41:19 +0300

    updated

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,8 +1,23 @@
 def pascal_triangle(n):
-    a=[1]
-    for i in range(1,n+1):
-      for k in range(1,i):
-         a.insert(i,k)
-      print(a)
+    if n > 0:
+        l = [0, 1, 0]
+        m = [[1]]
+        #print([1])
+        for x in range(n - 1):
+            k = []
+            for t in range(len(l)):
+                if t == 0:
+                    #print(t)
+                    k.append(l[0])
+                else:
+                    #print(t)
+                    k.append(l[t] + l[t-1])
 
-pascal_triangle(4)
+            k.append(l[t])
+            #m.append(k)
+            m.append(k[1:-1])
+            l = k
+        return m
+    else:
+        return []
+#pascal_triangle(5)
