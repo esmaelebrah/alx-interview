af3f48e2918a426ac39e5ba16e8c04d6075c4ead
esmaelebrah <esmaelebrahimhasengmail.com>
2024-05-16 19:35:59 +0300

    updated

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,42 +1,57 @@
 #!/usr/bin/python3
 """
-This module contains the function that displays the
-stats from the standard input
+Module Log Parsing
 """
-import re
 import sys
-status_codes = {200: 0, 301: 0, 400: 0, 401: 0,
-                403: 0, 404: 0, 405: 0, 500: 0}
-print_counter = 0
-size_summation = 0
 
 
-def print_logs():
+def print_dct(total_size, status_code):
     """
-    Prints status codes to the logs
+    print file size and status code
     """
-    print("File size: {}".format(size_summation))
-    for k, v in sorted(status_codes.items()):
-        if v != 0:
-            print("{}: {}".format(k, v))
+    print("File size: {}".format(total_size))
+    for key, value in sorted(status_code.items()):
+        if value != 0:
+            print("{}: {}".format(key, value))
+
+
+def main():
+    """
+    Main function
+    """
+    status_code = {
+        "200": 0,
+        "301": 0,
+        "400": 0,
+        "401": 0,
+        "403": 0,
+        "404": 0,
+        "405": 0,
+        "500": 0
+    }
+    total_size = 0
+    count = 0
+    try:
+        for line in sys.stdin:
+            count += 1
+            line = line.rsplit(None, 2)
+            if len(line) < 2:
+                continue
+            status, byte = line[1], line[2]
+            if status in status_code:
+                status_code[status] += 1
+            try:
+                total_size += int(byte)
+            except ValueError:
+                continue
+            if count % 10 == 0:
+                print_dct(total_size, status_code)
+        print_dct(total_size, status_code)
+
+    except KeyboardInterrupt:
+        print_dct(total_size, status_code)
+        raise
 
 
 if __name__ == "__main__":
-    try:
-        for line in sys.stdin:
-            std_line = line.replace("\n", "")
-            log_list = re.split('- | "|" | " " ', str(std_line))
-            try:
-                codes = log_list[-1].split(" ")
-                if int(codes[0]) not in status_codes.keys():
-                    continue
-                status_codes[int(codes[0])] += 1
-                print_counter += 1
-                size_summation += int(codes[1])
-                if print_counter % 10 == 0:
-                    print_logs()
-            except():
-                pass
-        print_logs()
-    except KeyboardInterrupt:
-        print_logs()
+    main()
